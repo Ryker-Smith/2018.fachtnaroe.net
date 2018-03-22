02f85e08031fa404803b54c62be94514cdcfce1e
steveyc95 <srcullen95@gmail.com>
2018-03-22 12:52:51 +0000

    Fix Container Size (Attempt 3)

--- html/resolution.js
+++ html/resolution.js
@@ -1,7 +1,7 @@
 function getResolution()
 {
-	var objectWidth = (document.getElementById("elem").width;
-	var objectHeight = (document.getElementById("elem").height;
+	var objectWidth = document.getElementById("elem").width;
+	var objectHeight = document.getElementById("elem").height;
 
 	document.getElementById("container").style.width = screen.width - objectWidth;
 	document.getElementById("container").style.height = screen.height - objectHeight;
