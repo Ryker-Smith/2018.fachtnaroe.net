9142f6f3a81fc74f50ee9667e6a6ebecb4a7a809
steveyc95 <srcullen95@gmail.com>
2018-03-22 14:00:48 +0000

    Resolution fix (Attempt 2)

--- html/resolution.js
+++ html/resolution.js
@@ -1,10 +1,10 @@
+var myObject = document.getElementById("elem");
+
+var objectWidth = myObject.width;
+var objectHeight = myObject.height;
+
 function getResolution()
 {
-	var myObject = document.getElementById("elem");
-
-	var objectWidth = myObject.width;
-	var objectHeight = myObject.height;
-
 	document.getElementById("container").style.width = screen.width - objectWidth;
 	document.getElementById("container").style.height = screen.height - objectHeight;
 }
