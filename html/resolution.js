5df6a831c4b24b81f4ffd4d584a7f1d14c02626c
steveyc95 <srcullen95@gmail.com>
2018-03-22 13:59:33 +0000

    Trying to fix resolution.js

--- html/resolution.js
+++ html/resolution.js
@@ -1,7 +1,9 @@
 function getResolution()
 {
-	var objectWidth = document.getElementById("elem").width;
-	var objectHeight = document.getElementById("elem").height;
+	var myObject = document.getElementById("elem");
+
+	var objectWidth = myObject.width;
+	var objectHeight = myObject.height;
 
 	document.getElementById("container").style.width = screen.width - objectWidth;
 	document.getElementById("container").style.height = screen.height - objectHeight;
