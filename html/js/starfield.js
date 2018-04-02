8d0578d4110ee38d82c946ef2b3dde2cd19ad295
Ryker-Smith <fachtnaroe@gmail.com>
2018-04-02 15:32:07 +0000

    Animation canvas stack applied to index file. (Front, rear layers working)

--- html/js/starfield.js
+++ html/js/starfield.js
@@ -16,6 +16,7 @@ var StarField=function (canvas_id, width, height, num_stars, star_size, stars) {
     star.x = Math.random() * width - origin_x;
     star.y = Math.random() * height - origin_y;
     star.z = star.max_depth = Math.max(width, height);
+    star.size=star_size;
     var xcoeff = star.x > 0 ? 1 : -1;
     var ycoeff = star.y > 0 ? 1 : -1;
     if (Math.abs(star.x) > Math.abs(star.y)) {
@@ -56,7 +57,7 @@ var StarField=function (canvas_id, width, height, num_stars, star_size, stars) {
       else {
         var circle = new Path2D();
         circle.moveTo(stars[i].x + origin_x, stars[i].y + origin_y);
-        circle.arc(stars[i].x + origin_x, stars[i].y + origin_y, star_size, 0, 2 * Math.PI);
+        circle.arc(stars[i].x + origin_x, stars[i].y + origin_y, stars[i].size, 0, 2 * Math.PI);
         ctx.fill(circle);
       }
     }
