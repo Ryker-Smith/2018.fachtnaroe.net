cc29cbc12587b77790e461e6745a1dec50595ab8
Ryker-Smith <fachtnaroe@gmail.com>
2018-04-03 14:01:37 +0000

    Re-enabled moving stars. Added more quotes.

--- html/js/randomQuote.js
+++ html/js/randomQuote.js
@@ -7,10 +7,24 @@ function randomQuote (displayDiv, quoteNumber, prefix) {
     // initialize quotes list. Quotes use ::: to separate words from speaker
     var quotes=[
         "Survive, stand like a rock; that the waters change course around you, and others without distraction may see further.",
+        "Coding is one of the highest art forms achieved by mankind; an idea - just an electrical impulse - is made real in an alternate electronic universe; the created idea however, does not have physical form, and yet can change the physical world; it cannot be touched, and yet may touch all mankind.",
         "The one thing that doesn’t abide by majority rule is a person’s conscience.:::Harper Lee",
         "Many people, especially ignorant people, want to punish you for speaking the truth, for being correct, for being you. Never apologize for being correct, or for being years ahead of your time. If you’re right and you know it, speak your mind. Speak your mind. Even if you are a minority of one, the truth is still the truth.:::Mahatma Gandhi",
         "Anti-social behaviour is a trait of intelligence in a world full of conformists.:::Nikola Tesla",
-        "Coding is one of the highest art forms achieved by mankind; an idea - just an electrical impulse - is made real in an alternate electronic universe; the created idea however, does not have physical form, and yet can change the physical world; it cannot be touched, and yet may touch all mankind."
+        "In the End, we will remember not the words of our enemies, but the silence of our friends.:::Martin Luther King, Jr.",
+        "Nothing in all the world is more dangerous than sincere ignorance and conscientious stupidity.:::Martin Luther King, Jr.",
+        "The function of education is to teach one to think intensively and to think critically. Intelligence plus character - that is the goal of true education.:::Martin Luther King, Jr.",
+        "Our lives begin to end the day we become silent about things that matter.:::Martin Luther King, Jr.",
+        "Never forget that everything Hitler did in Germany was legal.:::Martin Luther King, Jr.",
+        "Humans are allergic to change. They love to say, &quot;We've always done it this way.&quot; I try to fight that. That's why I have a clock on my wall that runs counter-clockwise.:::Rear Admiral Grace Murray Hopper",
+        "Life was simple before World War II. After that, we had systems.:::Rear Admiral Grace Murray Hopper",
+        "If it's a good idea, go ahead and do it. It is much easier to apologize than it is to get permission.:::Rear Admiral Grace Murray Hopper",
+        "A great empire, like a great cake, is most easily diminished at the edges.:::Benjamin Franklin",
+        "We are all born ignorant, but one must work hard to remain stupid.:::Benjamin Franklin",
+        "A house is not a home unless it contains food and fire for the mind as well as the body.:::Benjamin Franklin",
+        "Thousands of candles can be lighted from a single candle, and the life of the candle will not be shortened. Happiness never decreases by being shared.:::Buddha",
+        "The mind is everything. What you think you become.:::Buddha"
+
     ];
     // randomly choose a quote, or use quoteNumber provided (if in range)
     if ((quoteNumber < 0) || (quoteNumber > quotes.length)) {
---
+++ html/js/starfield-backup-02.js
@@ -0,0 +1,93 @@
+var StarField=function (canvas_id, width, height, num_stars, star_size, stars) {
+
+  canvas = document.getElementById(canvas_id);
+  ctx = canvas.getContext('2d');
+
+  width = width ? width : canvas.width,
+  star_size = star_size ? star_size : 2,
+  height = height ? height : canvas.height,
+  origin_x = width / 2,
+  origin_y = height / 2,
+  num_stars = num_stars ? num_stars : 80;
+      
+//   this.stars = [];
+  create_star=function () {
+    star = {};
+    star.x = Math.random() * width - origin_x;
+    star.y = Math.random() * height - origin_y;
+    star.z = star.max_depth = Math.max(width, height);
+    star.size=star_size;
+//     star.thing=thing;
+    var xcoeff = star.x > 0 ? 1 : -1;
+    var ycoeff = star.y > 0 ? 1 : -1;
+    if (Math.abs(star.x) > Math.abs(star.y)) {
+      star.dx = 1.0;
+      star.dy = Math.abs(star.y / star.x);
+    } else {
+      star.dx = Math.abs(star.x / star.y);
+      star.dy = 1.0;
+    }
+    star.dx *= xcoeff;
+    star.dy *= ycoeff;
+    star.dz = -1;
+    // rates of change
+    star.ddx = Math.random() * star.dx;
+    star.ddy = Math.random() * star.dy;
+    star.width = 1;
+    return star;
+  }
+  
+  move=function (star) {
+    star.x += star.dx;
+    star.y += star.dy;
+    star.z += star.dz;    
+    star.dx += star.ddx;
+    star.dy += star.ddy;
+    star.width = 1 + ((star.max_depth - star.z) * .1);
+  }
+  
+  function update_stars (ctx, stars) {
+    ctx.fillStyle = '#fff';
+    for (var i = 0; i < stars.length; i++) {
+      move(stars[i]);
+      if (stars[i].x < -origin_x || stars[i].x > origin_x ||
+          stars[i].y < -origin_y || stars[i].y > origin_y) {
+        // remove
+        stars[i] = create_star();
+      }
+      else {
+//           if (stars[i].thing.localeCompare("img")!=0) {
+// //               console.log(stars[i].thing);
+//             var circle = new Path2D();
+//             circle.moveTo(stars[i].x + origin_x, stars[i].y + origin_y);
+//             circle.arc(stars[i].x + origin_x, stars[i].y + origin_y, stars[i].size, 0, 2 * Math.PI);
+//             ctx.fill(circle);
+//           }
+//           else {
+//               console.log(stars[i].thing);
+            var image=document.createElement("img");
+            image.src="images/OneStar.png";
+            ctx.drawImage(image, 0, 0, image.width, image.height, stars[i].x + origin_x, stars[i].y + origin_y, image.width/3, image.height/3);
+//           }
+      }
+    }
+  }
+  
+  // drawing routine
+  this.draw = function(canvas_id) {
+      canvas = document.getElementById(canvas_id);
+      ctx = canvas.getContext('2d');
+//     ctx.fillStyle = "#fff";
+//     ctx.fillRect(0, 0, width, height);
+      ctx.clearRect(0, 0, width, height);
+//       console.log(canvas_id, thing);
+      update_stars(ctx, stars);
+    t = setTimeout(function(){draw(canvas_id)}, 30);
+  }
+  
+  for (var i=0; i < num_stars; i++) {
+    stars.push(create_star());
+  }
+  return this;
+}
+ 
