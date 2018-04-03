95f4a838a276ace069dd3800a10c7010cb18aa27
Ryker-Smith <fachtnaroe@gmail.com>
2018-04-03 15:15:03 +0000

    Varying colour stars

--- html/js/starfield.js
+++ html/js/starfield.js
@@ -1,5 +1,5 @@
-var StarField=function (canvas_id, width, height, num_stars, star_size, stars) {
-
+var StarField=function (canvas_id, width, height, num_stars, star_size) {
+  stars=stars_list;
   canvas = document.getElementById(canvas_id);
   ctx = canvas.getContext('2d');
 
@@ -8,16 +8,15 @@ var StarField=function (canvas_id, width, height, num_stars, star_size, stars) {
   height = height ? height : canvas.height,
   origin_x = width / 2,
   origin_y = height / 2,
-  num_stars = num_stars ? num_stars : 80;
+  num_stars = num_stars ? num_stars : 10;
       
-//   this.stars = [];
-  create_star=function () {
+  create_star=function (canvas_id) {
     star = {};
     star.x = Math.random() * width - origin_x;
     star.y = Math.random() * height - origin_y;
     star.z = star.max_depth = Math.max(width, height);
     star.size=star_size;
-//     star.thing=thing;
+    star.canvas_id=canvas_id;
     var xcoeff = star.x > 0 ? 1 : -1;
     var ycoeff = star.y > 0 ? 1 : -1;
     if (Math.abs(star.x) > Math.abs(star.y)) {
@@ -46,14 +45,15 @@ var StarField=function (canvas_id, width, height, num_stars, star_size, stars) {
     star.width = 1 + ((star.max_depth - star.z) * .1);
   }
   
-  function update_stars (ctx, stars) {
+  function update_stars (ctx) {
+
     ctx.fillStyle = '#fff';
     for (var i = 0; i < stars.length; i++) {
-      move(stars[i]);
+      this.move(stars[i]);
       if (stars[i].x < -origin_x || stars[i].x > origin_x ||
           stars[i].y < -origin_y || stars[i].y > origin_y) {
         // remove
-        stars[i] = create_star();
+        stars[i] = this.create_star(stars[i].canvas_id);
       }
       else {
 //           if (stars[i].thing.localeCompare("img")!=0) {
@@ -66,7 +66,17 @@ var StarField=function (canvas_id, width, height, num_stars, star_size, stars) {
 //           else {
 //               console.log(stars[i].thing);
             var image=document.createElement("img");
-            image.src="images/OneStar.png";
+            image.src="images/rockRed.png";
+            mt="canvas_middle";
+//             console.log(stars[i].canvas_id);
+            if (mt.localeCompare(stars[i].canvas_id)==0) {
+                image.src="images/rockRed.png";
+//                 console.log("blue");
+            }
+            else {
+                image.src="images/rockBlue.png";
+//                 console.log("red");
+            }
             ctx.drawImage(image, 0, 0, image.width, image.height, stars[i].x + origin_x, stars[i].y + origin_y, image.width/3, image.height/3);
 //           }
       }
@@ -81,12 +91,12 @@ var StarField=function (canvas_id, width, height, num_stars, star_size, stars) {
 //     ctx.fillRect(0, 0, width, height);
       ctx.clearRect(0, 0, width, height);
 //       console.log(canvas_id, thing);
-      update_stars(ctx, stars);
-    t = setTimeout(function(){draw(canvas_id)}, 30);
+      update_stars(ctx);
+    t = setTimeout(function(){draw(canvas_id)}, 40);
   }
   
   for (var i=0; i < num_stars; i++) {
-    stars.push(create_star());
+    stars_list.push(this.create_star(canvas_id));
   }
   return this;
 }
