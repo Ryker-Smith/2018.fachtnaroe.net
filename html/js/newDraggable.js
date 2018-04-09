b463439ed8e6f470988e3e32738228f6a82281bc
Ryker-Smith <fachtnaroe@gmail.com>
2018-04-09 14:08:19 +0100

    Changed draggable divs back to being created by JS

--- html/js/newDraggable.js
+++ html/js/newDraggable.js
@@ -1,21 +1,22 @@
 
-function newDraggable (elementParent, elementId, w, h, t) {
+function newDraggable (elementParent, elementId, w, h, im) {
   // Purpose: Simplify adding new draggable div
   //
-  // use (general): newDraggable (id_of_container, id_of_new_element, width_of_new_in_px, height_of_new_in_px, some_text_to_start_with)
-  // use (example): newDraggable ("container", "mynewthing", 200, 100, "Hello world");
+  // use (general): newDraggable (id_of_container, id_of_new_element, width_of_new_in_px, height_of_new_in_px, image_name)
+  // use (example): newDraggable ("container", "mynewthing", 200, 100, "image_name.jpg");
   
   // get a handle to the parent object
   var popsicle = document.getElementById(elementParent);
   // instantiate some objects that will be children
   var newDiv = document.createElement("div");
-  var newDiv_inner = document.createElement("div");
+  var newDiv_inner = document.createElement("img");
   // set the elementId
   newDiv.id=elementId;
   // provide width, height
   newDiv.style.width= w + "px";
   newDiv.style.height=h + "px";
   // set-up the mouse events
+  // onmousedown='mydragg.startMoving(this,"container",event);' onmouseup='mydragg.stopMoving("container");' style="width: 100px;height: 100px; background-color: transparent;"
   newDiv.onmousedown=function(event) {
     mydragg.startMoving(this, elementParent, event);
   };
@@ -23,15 +24,15 @@ function newDraggable (elementParent, elementId, w, h, t) {
     mydragg.stopMoving(elementParent);
   };
   // provide a bunch of CSS stuff at once; this should NOT be here
-  newDiv.style.cssText="position:absolute;background-color:green; -webkit-user-select:none; -moz-user-select:none; -o-user-select:none; -ms-user-select:none; -khtml-user-select:none; user-select:none;";
+  newDiv.style.cssText="position:absolute;background-color:transparent; -webkit-user-select:none; -moz-user-select:none; -o-user-select:none; -ms-user-select:none; -khtml-user-select:none; user-select:none;";
   // add an inner element, give it an id with _inner attached
   newDiv_inner.id=elementId + "_inner";
   // bulk-set CSS stuff at once; again, should NOT be here, prevents recursion
-  newDiv_inner.style.cssText="width:100%;height:100%;padding:10px;background-color:red;";
-  setRandomColor(newDiv);
-  setRandomColor(newDiv_inner);
+  newDiv_inner.style.cssText="width:" + w + "px;height:" + h + "px;background-color:transparent;  padding:10px;";
+//   setRandomColor(newDiv);
+//   setRandomColor(newDiv_inner);
   // insert the starting text provided
-  newDiv_inner.innerHTML=t;
+  newDiv_inner.src=im;
   // add the new inner div to the new child
   newDiv.appendChild(newDiv_inner);
   // add the new outer div to the parent as child
